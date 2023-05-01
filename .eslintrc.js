1f9974175abc874bf6ecab17b8b7cc2187399f7d
zetsuboii <hmzkrb@gmail.com>
2023-05-01 18:01:44 +0300

    init: config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,54 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   root: true,
-  extends: ['universe/native', 'universe/web'],
-  ignorePatterns: ['build'],
+  env: {
+    node: true,
+    jest: true,
+  },
+  // build folder comes from default Expo eslint config
+  ignorePatterns: [".eslintrc.js", "build"],
+  rules: {
+    "@typescript-eslint/array-type": ["error", { default: "generic" }],
+    "@typescript-eslint/explicit-module-boundary-types": "warn",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/ban-ts-comment": [
+      "warn",
+      {
+        "ts-expect-error": "allow-with-description",
+      },
+    ],
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: ["variable", "function"],
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+        leadingUnderscore: "allow",
+      },
+    ],
+    "@typescript-eslint/no-for-in-array": "error",
+    "@typescript-eslint/restrict-plus-operands": "error",
+    "@typescript-eslint/no-require-imports": "warn",
+    "@typescript-eslint/switch-exhaustiveness-check": "error",
+    "@typescript-eslint/promise-function-async": "warn",
+    "@typescript-eslint/consistent-type-imports": "warn",
+
+    // Extension Rules
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+
+    "no-throw-literal": "off",
+    "@typescript-eslint/no-throw-literal": "error",
+  },
 };
