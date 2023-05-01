e9738060519a200184e4ad7e7eb5bca57de5ac8e
zetsuboii <hmzkrb@gmail.com>
2023-05-02 00:32:17 +0300

    chore: tab to spaces

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,39 @@
 import SecureEnclaveModule from './SecureEnclaveModule';
 
 /**
-	@returns Public key for the alias
-	@dev Prompts for biometrics
-	@throws If there's no public key for the alias
+    @returns Public key for the alias
+    @dev Prompts for biometrics
+    @throws If there's no public key for the alias
 */
 export async function getPublicKey(alias: string): Promise<string> {
     return await SecureEnclaveModule.getPublicKey(alias);
 }
 
 /**
-	@description Generates a key pair for a alias
+    @description Generates a key pair for a alias
     @dev If there's a pair already, this function will override the existing one
-	@dev Prompts for biometrics
-	@returns Key pair's public key
-	@throws If key generation fails
+    @dev Prompts for biometrics
+    @returns Key pair's public key
+    @throws If key generation fails
 */
 export async function generateKeyPair(alias: string): Promise<string> {
     return await SecureEnclaveModule.generateKeyPair(alias);
 }
 
 /**
-	@description Deletes a key pair corresponding to a alias
-	@dev Prompts for biometrics
-	@throws If there's no key pair for the alias
+    @description Deletes a key pair corresponding to a alias
+    @dev Prompts for biometrics
+    @throws If there's no key pair for the alias
 */
 export async function deleteKeyPair(alias: string): Promise<void> {
     return await SecureEnclaveModule.deleteKeyPair(alias);
 }
 
 /**
-	@description Signs a message using key pair of the alias
-	@dev Prompts for biometrics
-	@throws If there's no key pair for the alias
-	@throws If signing fails
+    @description Signs a message using key pair of the alias
+    @dev Prompts for biometrics
+    @throws If there's no key pair for the alias
+    @throws If signing fails
 */
 export async function signMessage(
     alias: string,
