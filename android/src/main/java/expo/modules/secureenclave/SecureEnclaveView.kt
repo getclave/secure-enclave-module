add1d268a5098cfe825db8b302292f019003103c
zetsuboii <hmzkrb@gmail.com>
2023-05-01 21:56:23 +0300

    feat: make module ios only

--- android/src/main/java/expo/modules/secureenclave/SecureEnclaveView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.secureenclave
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class SecureEnclaveView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
