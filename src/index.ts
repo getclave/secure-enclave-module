87e1fa3078d171d13e8e28aef155e1f9dbc26920
zetsuboii <hmzkrb@gmail.com>
2023-05-01 21:55:24 +0300

    refactor: rm unused code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,6 @@
-import {
-    EventEmitter,
-    NativeModulesProxy,
-    Subscription,
-} from 'expo-modules-core';
-
-import {
-    ChangeEventPayload,
-    SecureEnclaveViewProps,
-} from './SecureEnclave.types';
 // Import the native module. On web, it will be resolved to SecureEnclave.web.ts
 // and on native platforms to SecureEnclave.ts
 import SecureEnclaveModule from './SecureEnclaveModule';
-import SecureEnclaveView from './SecureEnclaveView';
-
-// Get the native constant value.
-export const PI = SecureEnclaveModule.PI;
-
-export function hello(): string {
-    return SecureEnclaveModule.hello();
-}
-
-export async function setValueAsync(value: string) {
-    return await SecureEnclaveModule.setValueAsync(value);
-}
 
 /**
 	@returns Public key for the alias
@@ -65,15 +43,3 @@ export async function signMessage(
 ): Promise<string> {
     return await SecureEnclaveModule.signMessage(alias, message);
 }
-
-const emitter = new EventEmitter(
-    SecureEnclaveModule ?? NativeModulesProxy.SecureEnclave,
-);
-
-export function addChangeListener(
-    listener: (event: ChangeEventPayload) => void,
-): Subscription {
-    return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { SecureEnclaveView, SecureEnclaveViewProps, ChangeEventPayload };
